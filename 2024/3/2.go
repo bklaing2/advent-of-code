0ce9ea7333386e766b571006675030a9cc929029
Bryceson Laing <bklaing2@gmail.com>
2024-12-03 07:49:16 -0600

    day 3 part 2

--- 2024/3/2.go
+++ 2024/3/2.go
@@ -19,19 +19,27 @@ func main() {
 
   s := string(b)
 
+  r, _ := regexp.Compile(`mul\(\d+,\d+\)|do(n\'t)?\(\)`)
+  instSlice := r.FindAllString(s, -1)
+  inst := strings.Join(instSlice, " ")
 
-  r, _ := regexp.Compile(`mul\(\d+,\d+\)`)
-  mulsSlice := r.FindAllString(s, -1)
-  muls := strings.Join(mulsSlice, " ")
-
-  r, _ = regexp.Compile(`\d+`)
-  numsSlice := r.FindAllString(muls, -1)
+  r, _ = regexp.Compile(`\d+|do(n\'t)?\(\)`)
+  numsSlice := r.FindAllString(inst, -1)
 
   sum := 0
-  for i := 0; i < len(numsSlice) - 1; i += 2 {
-    a, _ := strconv.Atoi(numsSlice[i])
-    b, _ := strconv.Atoi(numsSlice[i + 1]) 
-    sum += a * b
+  doFlag := true
+  for i := 0; i < len(numsSlice); i++ {
+    inst := numsSlice[i]
+    if inst == "do()" {
+      doFlag = true
+    } else if inst == "don't()" {
+      doFlag = false
+    } else if doFlag {
+      a, _ := strconv.Atoi(numsSlice[i])
+      b, _ := strconv.Atoi(numsSlice[i + 1])
+      sum += a * b
+      i++
+    }
   }
 
   fmt.Println(sum)
